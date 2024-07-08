93c4e637eede54de9a9ceabcbf22fcdecbea354a
AbstractniBerezozol <aleksandr.zolotarev@abstract.rs>
2024-07-08 09:33:52 +0200

    initial commit

---
+++ src/users/entities/users.entitity.ts
@@ -0,0 +1,9 @@
+export class Users {
+    id: number;
+    nickname: string;
+    password: string;
+    email: string;
+    gitRepsList: string[]; 
+
+
+}
---
+++ src/users/users.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@nestjs/common';
+import { Users } from './entities/users.entitity';
+
+@Injectable()
+export class UsersService {
+  private users: Users[] = [
+    {
+      id: 1,
+      nickname: 'Lev',
+      email: 'At@at',
+      password: 'tritata',
+      gitRepsList: ['tri', 'cetire'],
+    },
+  ];
+
+  findAll() {
+    return this.users;
+  }
+
+  findOne(id: string) {
+    return this.users.find((item) => item.id === +id);
+  }
+
+  create(createUserDto: any) {
+    this.users.push(createUserDto);
+  }
+
+  update(id: string, updateCoffeeDto: any) {
+    const existingUser = this.findOne(id);
+    if (existingUser) {
+      // update the existing entity
+    }
+  }
+
+  remove(id: string) {
+    const userIndex = this.users.findIndex((item) => item.id === +id);
+    if (userIndex >= 0) {
+      this.users.splice(userIndex, 1);
+    }
+  }
+}
